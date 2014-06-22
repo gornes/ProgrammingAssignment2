de759085cb7c78024e8696e6ba0b7095b6694abc
Dejan Miljkovic <dejanm@gmail.com>
2014-06-22 12:11:59 -0700

    R Programming PA2

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,29 @@
-## ovo je test
-
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## Creates matrix object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+ m<-NULL
+ set <- function(y) {
+       x <<- y
+            m <<- NULL
+      }
+      get <- function() x
+      setinverse <- function(solve) m <<- solve
+      getinverse <- function() m
+      list(set = set, get = get,
+       setinverse = setinverse,
+            getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Computes the inverse of matrix object returned by makeCacheMatrix
+## If inverse has been calculated, it is retrieved from the cache
+cacheSolve <- function(x = matrix(), ...) {
+ ## Return a matrix that is the inverse of 'x'
+      m <- x$getinverse()
+      if(!is.null(m)) {
+       message("getting cached inverse")
+            return(m)
+      }
+      matrix <- x$get()
+      m <- solve(matrix, ...)
+      x$setinverse(m)
+      m
 }
